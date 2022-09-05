5da4b108433b90fd0e004bad160d04551ae728a6
Favour-dgreat <53022031+Favour-dgreat@users.noreply.github.com>
2022-09-05 16:15:09 +0100

    update url

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -6,7 +6,7 @@
 export default {
   "title": "The Complete Hub - Providing technical and educational resources for developers and tech enthusiasts",
   "tagline": "The Complete Hub (TC Hub) is a startup/community that is committed to providing technical content for everyone in the Tech Ecosystem, building a community of technical writers, and improving documentation, tutorial, and article content",
-  "url": "https://your-docusaurus-test-site.com",
+  "url": "https://thecompletehub.com",
   "baseUrl": "/",
   "onBrokenLinks": "throw",
   "onBrokenMarkdownLinks": "warn",
